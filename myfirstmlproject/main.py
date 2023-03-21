aa6a83eb585b3487df4c2158ba9c25a932af6112
Tanjim96 <58828226+Tanjim96@users.noreply.github.com>
2023-03-21 14:19:21 +0600

    Delete main.py

--- myfirstmlproject/main.py
+++
@@ -1,6 +0,0 @@
-import pandas
-from sklearn.linear_model import LinearRegression
-data = pandas.read_csv('iphone_price.csv')
-model = LinearRegression()
-model.fit(data[['version']], data[['price']])
-print(model.predict([[14]]))
